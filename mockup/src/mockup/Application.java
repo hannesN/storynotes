460c54527af5a12d347d5654b600244598c9ee09
Hannes Niederhausen <h.niederhausen@gmail.com>
2011-07-12 20:07:35 +0200

    added mockup using XWT and Windowbuilder Still Work to do

---
+++ mockup/src/mockup/Application.java
@@ -0,0 +1,34 @@
+package mockup;
+
+import java.net.URL;
+
+import org.eclipse.e4.xwt.IConstants;
+import org.eclipse.e4.xwt.XWT;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+public class Application {
+
+	public static void main(String args[]) throws Exception {
+		URL url = Application.class.getResource(Application.class
+				.getSimpleName() + IConstants.XWT_EXTENSION_SUFFIX);
+		Control control = XWT.load(url);
+		Shell shell = control.getShell();
+		shell.layout();
+		centerInDisplay(shell);
+		// run events loop
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!shell.getDisplay().readAndDispatch())
+				shell.getDisplay().sleep();
+		}
+	}
+
+	private static void centerInDisplay(Shell shell) {
+		Rectangle displayArea = shell.getDisplay().getClientArea();
+		shell.setBounds(displayArea.width / 4, displayArea.height / 4,
+				displayArea.width / 2, displayArea.height / 2);
+	}
+
+}
