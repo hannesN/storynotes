8f86b3ca4b76dcb7ec06e743a87c469a676fef8a
hannesN <h.niederhausen@gmail.com>
2011-09-07 21:00:00 +0200

    fixed #19: Specify CategoryService https://github.com/hannesN/storynotes/issues/issue/19

--- de.hannesniederhausen.storynotes.ui/src/de/hannesniederhausen/storynotes/ui/services/ICategoryProviderService.java
+++ de.hannesniederhausen.storynotes.ui/src/de/hannesniederhausen/storynotes/ui/services/ICategoryProviderService.java
@@ -3,9 +3,12 @@
  */
 package de.hannesniederhausen.storynotes.ui.services;
 
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.swt.widgets.Composite;
 
+import de.hannesniederhausen.storynotes.model.Category;
 import de.hannesniederhausen.storynotes.model.Note;
 import de.hannesniederhausen.storynotes.ui.views.InputMask;
 
@@ -39,14 +42,29 @@ public interface ICategoryProviderService {
 	 * An array of actions, which can be executed at the categories
 	 * navigation node.
 	 * 
+	 * @param context the {@link IEclipseContext} of the rcp application
+	 * @param parent the parent object
+	 * 
 	 * @return an array of actions. Must not be <code>null</code>
 	 */
-	public IAction[] getNoteActions();
+	public IAction[] getNoteActions(IEclipseContext context, EObject parent);
 	
 	/**
 	 * An array of actions, which will be added to the projects action list.
 	 * 
+	 * @param context the {@link IEclipseContext} of the rcp application
+	 * @param parent the parent object
+	 * 
 	 * @return an array of actions. Must not be <code>null</code>
 	 */
-	public IAction getCategoryActions();
+	public IAction getCategoryActions(IEclipseContext context, EObject parent);
+	
+	
+	/**
+	 * Returns the class object of the category which will be crated by this service. This is used to lookup the service
+	 * in the UI, e.g. when asking for input masks
+	 * 
+	 * @return the class object of the category
+	 */
+	public Class<? extends Category> getCategoryClass();
 }
