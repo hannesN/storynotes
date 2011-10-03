a210816a85e8fd077999b7ee28bb2dacd780cf6f
hannesN <h.niederhausen@gmail.com>
2011-10-03 11:50:14 +0200

    added undo/redo buttons to toolbar via application model fragment for #28 - does not work yet

---
+++ de.hannesniederhausen.storynotes.ui/src/de/hannesniederhausen/storynotes/ui/internal/handler/RedoHandler.java
@@ -0,0 +1,12 @@
+ 
+package de.hannesniederhausen.storynotes.ui.internal.handler;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class RedoHandler {
+	@Execute
+	public void execute() {
+		//TODO Your code goes here
+	}
+		
+}
---
+++ de.hannesniederhausen.storynotes.ui/src/de/hannesniederhausen/storynotes/ui/internal/handler/UndoHandler.java
@@ -0,0 +1,20 @@
+ 
+package de.hannesniederhausen.storynotes.ui.internal.handler;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class UndoHandler {
+	
+//	@Inject
+//	IEclipseContext context;
+//	
+	@Execute
+	public void execute() {
+		
+//		System.out.println(context);
+	}
+		
+}
