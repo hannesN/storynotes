0679b368191529acdba922fedb449c93fbf0cfa5
hannesN <h.niederhausen@gmail.com>
2011-10-08 16:08:49 +0200

    changed PersonCategoryInputMask ( #30 )

--- de.hannesniederhausen.storynotes.ui/src/de/hannesniederhausen/storynotes/ui/internal/services/ui/PersonCategoryInputMask.java
+++ de.hannesniederhausen.storynotes.ui/src/de/hannesniederhausen/storynotes/ui/internal/services/ui/PersonCategoryInputMask.java
@@ -3,12 +3,19 @@
  */
 package de.hannesniederhausen.storynotes.ui.internal.services.ui;
 
+import java.util.Arrays;
+
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.beans.PojoObservables;
 import org.eclipse.core.databinding.observable.Realm;
 import org.eclipse.core.databinding.observable.list.IObservableList;
 import org.eclipse.core.databinding.observable.map.IObservableMap;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.databinding.EMFProperties;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.command.RemoveCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.databinding.viewers.ObservableListContentProvider;
 import org.eclipse.jface.databinding.viewers.ObservableMapLabelProvider;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
@@ -16,13 +23,17 @@ import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
 
 import de.hannesniederhausen.storynotes.model.PersonCategory;
 import de.hannesniederhausen.storynotes.model.PersonNote;
+import de.hannesniederhausen.storynotes.model.StorynotesPackage;
 import de.hannesniederhausen.storynotes.ui.views.InputMask;
 
 /**
@@ -39,6 +50,9 @@ public class PersonCategoryInputMask extends InputMask implements IDoubleClickLi
 	private CheckboxTableViewer checkboxTableViewer;
 
 	
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	@Override
 	public void createControl(Composite parent) {
 		Composite comp = new Composite(parent, SWT.NONE);
@@ -49,8 +63,28 @@ public class PersonCategoryInputMask extends InputMask implements IDoubleClickLi
 		checkboxTableViewer.setColumnProperties(new String[] { "Name" });
 		checkboxTableViewer.setAllGrayed(false);
 		checkboxTableViewer.setAllChecked(false);
+		checkboxTableViewer.addDoubleClickListener(this);
 		table = checkboxTableViewer.getTable();
 		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		
+		Button deleteButton = new Button(comp, SWT.PUSH);
+		deleteButton.setText("Delete Selected Persons");
+		deleteButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				Object[] checkedElements = checkboxTableViewer.getCheckedElements();
+				if (checkedElements.length==0)
+					return;
+				
+				EditingDomain ed = getEclipseContext().get(EditingDomain.class);
+				
+				Command cmd = RemoveCommand.create(ed, category, StorynotesPackage.Literals.CATEGORY__NOTES, Arrays.asList(checkedElements));
+				
+				if (cmd.canExecute()) {
+					ed.getCommandStack().execute(cmd);
+				}
+			}
+		});
 		
 		setControl(comp);
 	}
@@ -71,13 +105,13 @@ public class PersonCategoryInputMask extends InputMask implements IDoubleClickLi
 		DataBindingContext bindingContext = new DataBindingContext();
 		//
 		ObservableListContentProvider listContentProvider = new ObservableListContentProvider();
+		
 		IObservableMap observeMap = PojoObservables.observeMap(listContentProvider.getKnownElements(), PersonNote.class, "name");
 		checkboxTableViewer.setLabelProvider(new ObservableMapLabelProvider(observeMap));
 		checkboxTableViewer.setContentProvider(listContentProvider);
 
 		//
-		IObservableList categoryNotesObserveList = PojoObservables.observeList(
-				Realm.getDefault(), category, "notes");
+		IObservableList categoryNotesObserveList = EMFProperties.list(StorynotesPackage.Literals.CATEGORY__NOTES).observe(category); 
 		checkboxTableViewer.setInput(categoryNotesObserveList);
 		//
 		return bindingContext;
@@ -87,6 +121,7 @@ public class PersonCategoryInputMask extends InputMask implements IDoubleClickLi
 	public void doubleClick(DoubleClickEvent event) {
 		IStructuredSelection sel = (IStructuredSelection) checkboxTableViewer.getSelection();
 		
-		// TODO set selection
+		ESelectionService eSelectionService = getEclipseContext().get(ESelectionService.class);
+		eSelectionService.setSelection(sel.getFirstElement());
 	}
 }
