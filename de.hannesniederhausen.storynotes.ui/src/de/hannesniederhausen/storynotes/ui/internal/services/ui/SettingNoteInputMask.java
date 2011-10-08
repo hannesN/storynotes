ae1c748723268f2678a8e68bab236229d8ad3084
hannesN <h.niederhausen@gmail.com>
2011-10-08 14:33:26 +0200

    changed SettingNoteInputMask ( #30 )

--- de.hannesniederhausen.storynotes.ui/src/de/hannesniederhausen/storynotes/ui/internal/services/ui/SettingNoteInputMask.java
+++ de.hannesniederhausen.storynotes.ui/src/de/hannesniederhausen/storynotes/ui/internal/services/ui/SettingNoteInputMask.java
@@ -4,9 +4,10 @@
 package de.hannesniederhausen.storynotes.ui.internal.services.ui;
 
 import org.eclipse.core.databinding.DataBindingContext;
-import org.eclipse.core.databinding.beans.PojoObservables;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.emf.databinding.edit.EMFEditProperties;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -17,6 +18,7 @@ import org.eclipse.swt.widgets.Text;
 
 import de.hannesniederhausen.storynotes.model.PlotNote;
 import de.hannesniederhausen.storynotes.model.SettingNote;
+import de.hannesniederhausen.storynotes.model.StorynotesPackage;
 import de.hannesniederhausen.storynotes.ui.views.InputMask;
 
 /**
@@ -83,18 +85,19 @@ public class SettingNoteInputMask extends InputMask {
 		setControl(comp);
 	}
 	protected DataBindingContext initDataBindings() {
+		EditingDomain ed = getEclipseContext().get(EditingDomain.class);
 		DataBindingContext bindingContext = new DataBindingContext();
 		//
 		IObservableValue nameTextObserveTextObserveWidget = SWTObservables.observeDelayedValue(300, SWTObservables.observeText(nameText, SWT.Modify));
-		IObservableValue modelTitleObserveValue = PojoObservables.observeValue(model, "name");
+		IObservableValue modelTitleObserveValue = EMFEditProperties.value(ed, StorynotesPackage.Literals.SETTING_NOTE__NAME).observe(model);
 		bindingContext.bindValue(nameTextObserveTextObserveWidget, modelTitleObserveValue, null, null);
 		//
 		IObservableValue textObserveTextObserveWidget = SWTObservables.observeDelayedValue(1000, SWTObservables.observeText(contentText, SWT.Modify));
-		IObservableValue modelDescriptionObserveValue = PojoObservables.observeValue(model, "description");
+		IObservableValue modelDescriptionObserveValue = EMFEditProperties.value(ed, StorynotesPackage.Literals.SETTING_NOTE__DESCRIPTION).observe(model);
 		bindingContext.bindValue(textObserveTextObserveWidget, modelDescriptionObserveValue, null, null);
 		//
 		IObservableValue typeTextObserveTextObserveWidget = SWTObservables.observeDelayedValue(1000, SWTObservables.observeText(typeText, SWT.Modify));
-		IObservableValue modelKindObserveValue = PojoObservables.observeValue(model, "kind");
+		IObservableValue modelKindObserveValue = EMFEditProperties.value(ed, StorynotesPackage.Literals.SETTING_NOTE__KIND).observe(model);
 		bindingContext.bindValue(typeTextObserveTextObserveWidget, modelKindObserveValue, null, null);
 		//
 		return bindingContext;
