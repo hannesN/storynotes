b8c9acf3f0cc35a79bb1ff2222a8555c0f78b0ba
hannesN <h.niederhausen@gmail.com>
2011-10-17 11:19:50 +0200

    added missing @CanExecute annotation to SaveHandler

--- de.hannesniederhausen.storynotes.ui/src/de/hannesniederhausen/storynotes/ui/internal/handler/SaveHandler.java
+++ de.hannesniederhausen.storynotes.ui/src/de/hannesniederhausen/storynotes/ui/internal/handler/SaveHandler.java
@@ -5,6 +5,7 @@ package de.hannesniederhausen.storynotes.ui.internal.handler;
 
 import javax.inject.Inject;
 
+import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 
 import de.hannesniederhausen.storynotes.model.File;
@@ -24,6 +25,7 @@ public class SaveHandler {
 		modelProviderService.saveFile();
 	}
 
+	@CanExecute
 	public boolean canExecute() {
 		File file = modelProviderService.getFile();
 		return file!=null && file.getFilename()!=null;
